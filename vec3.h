16cc74db7274e85c82889662b582a43138083c79
Tokiwa-17 <2872444509@qq.com>
2023-05-15 12:31:20 +0800

    fix

---
+++ vec3.h
@@ -0,0 +1,66 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+    public:
+
+        __host__ __device__ vec3() : e() {}
+        __host__ __device__ vec3(float e0, float e1, float e2) {
+            e[0] = e0; e[1] = e1; e[2] = e2;
+        }
+
+        __host__ __device__ float x() const { return e[0]; }
+        __host__ __device__ float y() const { return e[1]; }
+        __host__ __device__ float z() const { return e[2]; }
+
+        __host__ __device__ vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        __host__ __device__ float operator[](int i) const { return e[i]; }
+        __host__ __device__ float& operator[](int i) { return e[i]; }
+
+        __host__ __device__ vec3& operator+=(const vec3 &v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        __host__ __device__ vec3& operator-=(const vec3 &v) {
+            e[0] -= v.e[0];
+            e[1] -= v.e[1];
+            e[2] -= v.e[2];
+            return *this;
+        }
+
+        __host__ __device__ vec3& operator*=(const float t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        __host__ __device__ vec3& operator/=(const float t) {
+            return *this *= 1/t;
+        }
+
+        __host__ __device__ float length() const {
+            return sqrt(length_squared());
+        }
+
+        __host__ __device__ float length_squared() const {
+            return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+        }
+
+    public:
+        float e[3];
+};
+
+// Type aliases for vec3
+using point3 = vec3;   // 3D point
+using color = vec3;    // RGB color
+
+#endif
